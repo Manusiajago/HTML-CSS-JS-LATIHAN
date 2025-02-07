26e2f449adfb1be71803087264ba92be20327ed8
egal <egalassegaf@gmail.com>
2025-02-07 16:14:34 +0700

    membuat section apartment

--- 3. Web-Real-Estate/dist/js/main.js
+++ 3. Web-Real-Estate/dist/js/main.js
@@ -5,7 +5,12 @@ const bgNav = document.querySelector(".bg-navbar");
 menuBar.addEventListener("click", function () {
     menuNav.classList.toggle("menu-active");
     bgNav.classList.toggle("bgNav-active");
-})
+});
+
+// window.addEventListener('scroll', () => {
+//     menuNav.classList.remove("menu-active");
+//     bgNav.classList.remove("bgNav-active");
+// });
 
 bgNav.addEventListener('click', () => {
     menuNav.classList.remove("menu-active");

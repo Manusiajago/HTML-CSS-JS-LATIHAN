e88e48c1bc4c6cc0255c838f679779e512602295
egal <egalassegaf@gmail.com>
2025-07-21 22:30:18 +0700

    refactor code

--- 7. Membuat_Form_Pendaftaran/script.js
+++ 7. Membuat_Form_Pendaftaran/script.js
@@ -18,9 +18,9 @@ document.addEventListener('DOMContentLoaded', () => {
     // Validasi realtime ketika user meninggalkan input atau mengetik
     const inputs = form.querySelectorAll('.form-input');
     inputs.forEach(input => {
-        input.addEventListener('blur', (() => {
+        input.addEventListener('blur', function () {
             validateField(this)
-        }))
+        })
 
         input.addEventListener('input', (() => {
             clearError(this)
@@ -32,15 +32,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const confirmPasswordInput = document.getElementById('confirmPassword');
 
     //cek kecocokan password ketika user mengecek password
-    confirmPasswordInput.addEventListener('input', (() => {
+    confirmPasswordInput.addEventListener('input', function () {
         if (passwordInput.value && this.value) {
             if (passwordInput.value !== this.value) {
-                showError('this', "password tidak cocok");
+                showError(this, "password tidak cocok");
             } else {
                 clearError(this);
             }
         }
-    }));
+    });
 
     // penanganan klik tombol media sosial
     document.querySelector('.google-btn').addEventListener('click', function () {
@@ -59,15 +59,15 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 // fungsi untuk memvalidasi seluruh form
-const validateForm = (() => {
+function validateForm() {
     let isValid = true;
 
     //ambil nilai dari semua input form
-    const username = document.getElementById('username');
-    const fullname = document.getElementById('fullname');
-    const email = document.getElementById('email');
-    const password = document.getElementById('password');
-    const confirmPassword = document.getElementById('confirmPassword');
+    const username = document.getElementById('username').value.trim();
+    const fullname = document.getElementById('fullname').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value
+    const confirmPassword = document.getElementById('confirmPassword').value;
     const agreement = document.getElementById('agreement').checked;
 
     // validasi username
@@ -98,7 +98,7 @@ const validateForm = (() => {
 
     // validasi password
     if (!confirmPassword) {
-        showError(document.getElementById('password', "Password harus di isi"));
+        showError(document.getElementById('password'), "Password harus di isi");
         isValid = false;
     } else if (password !== confirmPassword) {
         showError(document.getElementById('password', "Password tidak cocok"));
@@ -113,7 +113,7 @@ const validateForm = (() => {
 
     return isValid;
 
-})
+}
 
 
 

e322ed6e023281ce27d61539117fdca36f678d00
egal <egalassegaf@gmail.com>
2025-07-21 21:47:44 +0700

    membuat validasi nama lengkap

---
+++ 7. Membuat_Form_Pendaftaran/script.js
@@ -0,0 +1,87 @@
+// Penanganan validasi dan pengiriman form
+document.addEventListener('DOMContentLoaded', () => {
+    // ambil elemen form dan tombol submit
+    const form = document.getElementById('registrationForm');
+    const submitBtn = document.querySelectorAll('.submit-btn');
+
+    // penanganan pengiriman form
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        // Jika validasi berhasil tampilkan pesan sukses
+        if (validateForm()) {
+            showSuccesMessage();
+            resetForm();
+        }
+    })
+
+    // Validasi realtime ketika user meninggalkan input atau mengetik
+    const inputs = form.querySelectorAll('.form-input');
+    inputs.forEach(input => {
+        input.addEventListener('blur', (() => {
+            validateField(this)
+        }))
+
+        input.addEventListener('input', (() => {
+            clearError(this)
+        }))
+    });
+
+    // pengecekan konfirmasi password secara realtime
+    const passwordInput = document.getElementById('password');
+    const confirmPasswordInput = document.getElementById('confirmPassword');
+
+    //cek kecocokan password ketika user mengecek password
+    confirmPasswordInput.addEventListener('input', (() => {
+        if (passwordInput.value && this.value) {
+            if (passwordInput.value !== this.value) {
+                showError('this', "password tidak cocok");
+            } else {
+                clearError(this);
+            }
+        }
+    }));
+
+    // penanganan klik tombol media sosial
+    document.querySelector('.google-btn').addEventListener('click', function () {
+        alert('Tombol Google diklik');
+    })
+
+    document.querySelector('.facebook-btn').addEventListener('click', function () {
+        alert('facebook akan segera tersedia')
+    })
+
+    // penangana klik aturan pakai
+    document.querySelector('.terms-link').addEventListener('click', function (e) {
+        e.preventDefault();
+        alert('Halaman aturan pakai akan segera tersedia');
+    })
+});
+
+// fungsi untuk memvalidasi seluruh form
+const validateForm = (() => {
+    let isValid = true;
+
+    //ambil nilai dari semua input form
+    const username = document.getElementById('username');
+    const fullname = document.getElementById('fullname');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const confirmPassword = document.getElementById('confirmPassword');
+    const agreement = document.getElementById('agreement').checked;
+
+    // validasi username
+    if (!username) {
+        showError(document.getElementById('username'), 'Username harus di isi');
+        isValid = false;
+    } else if (username.length < 3) {
+        showError(document.getElementById('username', 'Username minimal 3 karakter'));
+        isValid = false;
+    }
+
+    // lalu validasi nama lengkap
+
+})
+
+
+
